e96dedd6b5ea76272b9d07cc4ca33336012c3971
Noha Waheed <79611559+nohawaheed@users.noreply.github.com>
2022-02-12 23:43:10 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  let isFistSlide;
+  let isSecondSlide;
+  $(".arrows-container img").click(function (e) {
+    let btn = e.target.attributes.src.value;
+    let prevBtn = $(".prev-btn").attr("src");
+    let nextBtn = $(".next-btn").attr("src");
+
+    isFistSlide = $("figure.first-slide").css("display");
+    isSecondSlide = $("figure.second-slide").css("display");
+    if (
+      (btn == nextBtn && isFistSlide == "flex") ||
+      (btn == prevBtn && isFistSlide == "flex")
+    ) {
+      $("figure.first-slide").css({ display: "none" }, 500);
+      $("figure.second-slide").css({ display: "flex" }, 500);
+    }
+
+    if (
+      (btn == nextBtn && isSecondSlide == "flex") ||
+      (btn == prevBtn && isSecondSlide == "flex")
+    ) {
+      $("figure.second-slide").css({ display: "none" }, 500);
+      $("figure.first-slide").css({ display: "flex" }, 500);
+    }
+  });
+  $("body").keydown(function (e) {
+    isFistSlide = $("figure.first-slide").css("display");
+    isSecondSlide = $("figure.second-slide").css("display");
+    if (
+      (e.key == "ArrowRight" && isFistSlide == "flex") ||
+      (e.key == "ArrowLeft" && isFistSlide == "flex")
+    ) {
+      $("figure.first-slide").css({ display: "none" }, 500);
+      $("figure.second-slide").css({ display: "flex" }, 500);
+    }
+    if (
+      (e.key == "ArrowRight" && isSecondSlide == "flex") ||
+      (e.key == "ArrowLeft" && isSecondSlide == "flex")
+    ) {
+      $("figure.second-slide").css({ display: "none" }, 500);
+      $("figure.first-slide").css({ display: "flex" }, 500);
+    }
+    console.log(typeof e.key);
+  });
+});
